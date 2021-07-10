ffe8e6bc999a7de371202bd889f07c0f7fb5037f
touchavin <touchavin@hotmail.com>
2021-07-10 12:32:50 +0700

    first commit

--- mainheroku/settings.py
+++ mainheroku/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appheroku',
+    
 ]
 
 MIDDLEWARE = [
